959f990e832c5e4a1b11b37dcc82cbf52cb752a6
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-01-13 19:08:08 -0300

    operaciones listas

--- js/script.js
+++ js/script.js
@@ -5,21 +5,37 @@ let resultado = 0;
 
 teclas.forEach(tecla =>{
     tecla.addEventListener("click", ()=>{
+        if (displayResultado.textContent == 0) {
+            displayResultado.textContent = "";
+        }
+
         if (tecla.textContent == "AC") {
             console.log("apretaste ac")
             resultado = 0;
             displayResultado.innerHTML = resultado;
-            
+
         }else if (tecla.textContent == "DE") {
             
             displayResultado.textContent = displayResultado.textContent.slice(0,-1);
 
         }else if (tecla.textContent == "=") {
-            mostrarResultado();
+            realizarOperacion();
 
         }else{
             displayResultado.textContent += tecla.textContent;
         }
 
+        corroborarQueElCampoNoEsteVacio();
     })
 })
+
+function corroborarQueElCampoNoEsteVacio(){
+    if (displayResultado.textContent == "") {
+        displayResultado.textContent = 0;
+    }
+}
+
+function realizarOperacion(){
+    resultado = displayResultado.textContent.replace("x","*");
+    displayResultado.textContent = eval(resultado);
+}
